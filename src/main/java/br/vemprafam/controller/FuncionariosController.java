daa9ef7ee658537ce87d7ebea4705c59f63326a9
052787 <052787@SPFAM2LAB161-20.famacad.com.br>
2026-03-24 21:11:32 -0300

    Controller de funcionários

---
+++ src/main/java/br/vemprafam/controller/FuncionariosController.java
@@ -0,0 +1,37 @@
+package br.vemprafam.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.vemprafam.dao.DaoFuncionario;
+import br.vemprafam.pojo.Funcionario;
+
+@Controller
+@RequestMapping("/funcionarios")
+public class FuncionariosController {
+
+	@GetMapping
+	String showFuncionariosHomeVazio(Model model) {
+		return "funcionarios";
+	}
+	@GetMapping("/")
+	String showFuncionariosHome(Model model) {
+		return "funcionarios";
+	}
+	@GetMapping("/new")
+	public String showFormFuncionarios(Model model) {
+		Funcionario func = new Funcionario();
+		model.addAttribute("funcionario", func );
+		return "create-funcionario";
+	}
+	DaoFuncionario dao = new DaoFuncionario();
+	@PostMapping
+	public String insert(@ModelAttribute Funcionario func) {
+		dao.insert(func);
+		return "redirect:funcionarios";
+	}
+}
