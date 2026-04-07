f78c80920a13c0e416b098e86d4ba2421e3f0a08
052787 <052787@SPFAM2LAB161-20.famacad.com.br>
2026-04-07 20:15:32 -0300

    exclusão e alteração de funcionarios

--- src/main/java/br/vemprafam/controller/FuncionariosController.java
+++ src/main/java/br/vemprafam/controller/FuncionariosController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import br.vemprafam.dao.DaoFuncionario;
 import br.vemprafam.pojo.Funcionario;
@@ -41,5 +42,31 @@ public class FuncionariosController {
 		List<Funcionario> funcionarios = dao.getLista();
 		model.addAttribute("funcionarios", funcionarios);
 		return "funcionarios-list";
+	}
+	@GetMapping("/excluir")
+	public String showExcluir() {
+		return "excluir-funcionario";
+	}
+	@GetMapping("/delete")
+	public String delete(@RequestParam int re) {
+		Funcionario f = dao.buscarPeloRe(re);
+		dao.delete(f);
+		return "funcionarios";
+	}
+	@GetMapping("/busca")
+	public String showBusca() {
+		return "buscar-funcionario";
+	}
+
+	@GetMapping("/showUpdate")
+	public String showUpdate(@RequestParam int re, Model model) {
+		Funcionario funcionario = dao.buscarPeloRe(re);
+		model.addAttribute("funcionario", funcionario);
+		return "alterar-funcionario";
+	}
+	@PostMapping("/update")
+	public String update(@ModelAttribute Funcionario funcionario) {
+		dao.update(funcionario);
+		return "funcionarios";
 	}
 }
